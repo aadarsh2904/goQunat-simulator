a4bfef23dfa33f1350e6d20fb8d37e86fbadb5de
xenom2004 <117723390+xenom2004@users.noreply.github.com>
2025-05-16 14:56:06 +0530

    fetch market

--- fetch-apis/first.py
+++ fetch-apis/first.py
@@ -1,1 +1,11 @@
-print("hello worldss")
+import okx.MarketData as MarketData
+
+flag = "0"  # Production trading:0 , demo trading:1
+
+marketDataAPI =  MarketData.MarketAPI(flag=flag)
+
+# Retrieve the latest price snapshot, best bid/ask price, and trading volume in the last 24 hours
+result = marketDataAPI.get_tickers(
+    instType="SWAP"
+)
+print(result)
